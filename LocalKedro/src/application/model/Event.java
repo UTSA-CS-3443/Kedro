f33033d13a1ae51db1319328062920974d2e97b4
Travis Woods <woods49warhorse@hotmail.com>
2017-10-31 16:11:45 -0500

    Minor updates and no errors

--- LocalKedro/src/application/model/Event.java
+++ LocalKedro/src/application/model/Event.java
@@ -2,7 +2,7 @@ package application.model;
 
 public class Event {
 
-	public Event(String name, Type type, Date date, Location location) {
+	public Event(String name, Type type, /*Date date,*/ Location location) {
 		//objects need to be created for each of these and maybe add some more info to this
 	}
 }
