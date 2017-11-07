542726ae5e888f1926e9b8c0e909d701a1ae5de0
Travis Woods <woods49warhorse@hotmail.com>
2017-11-07 13:57:52 -0600

    Last one I messed up. Heres the real commit with the controllers
    This adds the controller classes but read the last commit description on
    how to log in. firstname lastname and abc123 for password. I already
    added all 4 of us in

---
+++ LocalKedro/src/application/controller/EventController.java
@@ -0,0 +1,34 @@
+package application.controller;
+
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+/**
+ * This class is going to control the event page button presses and user input
+ * @author Travis
+ *
+ */
+public class EventController implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent event) { //press on the create event page
+		//name
+				//type
+				//date
+				//location
+				//compare emails to each other and password
+				try {
+					   Parent root = FXMLLoader.load(getClass().getResource("IdkIguess.fxml")); //replace this pls
+					   Main.stage.setScene(new Scene(root, 600, 400));
+					   Main.stage.show();
+					   //Event event = new Event(name, type, date, location);
+					} catch(Exception e) {
+					   e.printStackTrace();
+					}
+				System.out.println("event");		
+	}
+
+}
---
+++ LocalKedro/src/application/controller/UserController.java
@@ -0,0 +1,18 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+/**
+ * This will handle everything that does with the profile and user page
+ * @author Spacehawks
+ *
+ */
+public class UserController  implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
