2ee8e359adfd11fa0aea3c1b8c4cd587c6e233d2
brusnak89 <rhcp002743@gmail.com>
2017-11-12 17:15:14 -0600

    updated CreateEventController.java
    added text field handling

--- LocalKedro/src/application/controller/CreateEventController.java
+++ LocalKedro/src/application/controller/CreateEventController.java
@@ -6,24 +6,41 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 
 public class CreateEventController implements EventHandler<ActionEvent>{
 
 	//this class handles all of the Event controllers within each individual event page
 	//as in creating the event but also when the user interacts with an already created event page
 	//when join event is pressed it creates an rsvp object for the user
+	
+	public TextField ENAME;
+	public TextField ETYPE;
+	public TextField EDATE;
+	public TextField ELOC;
+	
+	String eName = "event name";
+	String eType = "event type";
+	String eDate = "event date";
+	String eLoc = "event location";
+	
 	@Override
 	public void handle(ActionEvent event) {
 		//takes info from the user and puts into event object in the try
+		eName = ENAME.getText().toString();
+		eType = ETYPE.getText().toString();
+		eDate = EDATE.getText().toString();
+		eLoc = ELOC.getText().toString();
 		try {
 			   Parent root = FXMLLoader.load(getClass().getResource("EventsPage.fxml"));
 			   Main.stage.setScene(new Scene(root, 600, 400));
 			   Main.stage.show();
-			   //Event event = new Event(name, type, date, location);
+			   //Event event = new Event(eName, eType, eDate, eLoc);
 		} 
 		catch(Exception e) {
 			   e.printStackTrace();
 		}
+		System.out.println(eName + "\n" + eType + "\n" + eDate + "\n" + eLoc + "\n");
 		System.out.println("Event Created");
 	}
 
