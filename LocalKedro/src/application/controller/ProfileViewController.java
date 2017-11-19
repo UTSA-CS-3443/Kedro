c0db3c8d9ea107da4458cd64e49241842c7a7f9d
brusnak89 <rhcp002743@gmail.com>
2017-11-19 15:15:42 -0600

    Update ProfileViewController.java
    added functionality

--- LocalKedro/src/application/controller/ProfileViewController.java
+++ LocalKedro/src/application/controller/ProfileViewController.java
@@ -1,13 +1,26 @@
 package application.controller;
 
+import java.util.ResourceBundle;
+
+import javax.annotation.Resources;
+import javax.print.DocFlavor.URL;
+
 import application.Main;
+import application.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
-public class ProfileViewController implements EventHandler<ActionEvent>{
+public class ProfileViewController implements Initializable,EventHandler<ActionEvent>{
+	
+	public Label user;
+	public Label name;
+	public Label zip;
+	public Label events;
 	
 	@Override
 	public void handle(ActionEvent event) {
@@ -30,5 +43,29 @@ public class ProfileViewController implements EventHandler<ActionEvent>{
 			e.printStackTrace();
 		}
 		System.out.println("Home Button");
+	}
+	
+
+	@Override
+	public void initialize(java.net.URL arg0, ResourceBundle arg1) {
+		if (Main.user.getName() == null){
+			user.setText("Username : ");
+		}
+		else{
+			user.setText("Username: " + Main.user.getName());
+		}
+		if (Main.user.getFName() == null){
+			name.setText("Name :");
+		}
+		else{
+			name.setText("Name : " + Main.user.getFName());
+		}
+		if(Main.user.getLoc() == null){
+			zip.setText("Zip Code : ");
+		}
+		else{
+			zip.setText("Zip Code : " + Main.user.getLoc());
+		}
+		events.setText("Recent Events : TEST");
 	}
 }
