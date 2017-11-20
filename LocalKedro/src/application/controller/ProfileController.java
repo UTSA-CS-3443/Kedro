1eb29a59b1452af4146d871aaf23e68d67ba29cf
brusnak89 <rhcp002743@gmail.com>
2017-11-19 19:33:06 -0600

    Update ProfileController.java
    added search not working as intended

--- LocalKedro/src/application/controller/ProfileController.java
+++ LocalKedro/src/application/controller/ProfileController.java
@@ -6,8 +6,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 
 public class ProfileController implements EventHandler<ActionEvent> {
+	
+	public TextField search;
+	String s;
 
 	@Override
 	public void handle(ActionEvent event) {
@@ -34,5 +38,16 @@ public class ProfileController implements EventHandler<ActionEvent> {
 			e.printStackTrace();
 		}
 		System.out.print("Viewing Personal Profile");
+	}
+	public void search(ActionEvent event){
+		s = search.getText().toString();
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("ProfileResultsPage.fxml"));
+			Main.stage.setScene(new Scene(root, 600, 400));
+			Main.stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("profile search");
 	}
 }
