15ec28d5ab4b8129e94c221afe2dddbef1818c8c
Travis Woods <woods49warhorse@hotmail.com>
2017-12-06 10:27:31 -0600

    Final project. Deleted miscellaneous files and fixed an error Nobody touch this. I am currently making the uml diagram

--- LocalKedro/src/application/model/EventSearch.java
+++ LocalKedro/src/application/model/EventSearch.java
@@ -99,7 +99,7 @@ public class EventSearch {
 		// reads in a folder of files
 		File folder = new File(filePath);
 		File[] listOfFiles = folder.listFiles();
-		DisplayEvent[] eventList = new DisplayEvent[listOfFiles.length];
+		DisplayEvent[] eventList = new DisplayEvent[listOfFiles.length - 1];
 		int i = 0;
 		// goes through all the files and searches their names
 		for (File rd : listOfFiles) {
